d228d4766f177848c303fc421a5a227c8285c0ba
Pawan201296 <pawansingh261094@gmail.com>
2025-02-10 21:01:17 +0530

    software page

---
+++ pages/softwares/chronos.js
@@ -0,0 +1,42 @@
+// MODULES //
+
+// COMPONENTS //
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import MetaTags from "@/components/MetaTags";
+
+// SECTIONS //
+import SoftwareBanner from "@/sections/softwares/SoftwareBanner";
+
+// PLUGINS //
+
+// UTILS //
+
+// STYLES //
+import styles from "@/styles/pages/softwares/Software.module.scss";
+
+// IMAGES //
+
+// DATA //
+
+/** Chronos Page */
+export default function SoftwarePage() {
+	return (
+		<div>
+			{/* Metatags */}
+			<MetaTags Title={"Chronos"} Desc={""} OgImg={""} Url={"/chronos"} />
+
+			{/* Header */}
+			{/* <Header /> */}
+
+			{/* Page Content starts here */}
+			<main className={styles.SoftwarePage}>
+				<SoftwareBanner />
+			</main>
+			{/* Page Content ends here */}
+
+			{/* Footer */}
+			{/* <Footer /> */}
+		</div>
+	);
+}
---
+++ src/sections/softwares/SoftwareBanner.js
@@ -0,0 +1,21 @@
+// MODULES //
+
+// COMPONENTS //
+
+// SECTIONS //
+
+// PLUGINS //
+
+// UTILS //
+
+// STYLES //
+import styles from "@/styles/sections/SoftwareBanner.module.scss";
+
+// IMAGES //
+
+// DATA //
+
+/** SoftwareBanner Section */
+export default function SoftwareBanner() {
+	return <section className={styles.SoftwareBanner}></section>;
+}
