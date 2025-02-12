fe86f86dd75c9ce2404ce36abce75d1b151a8bdd
VipVishal007 <mvishu405@gmail.com>
2025-02-13 01:07:22 +0530

    setup done

--- src/sections/softwares/SoftwareBanner.js
+++ src/sections/softwares/SoftwareBanner.js
@@ -9,13 +9,24 @@
 // UTILS //
 
 // STYLES //
-import styles from "@/styles/sections/SoftwareBanner.module.scss";
+import styles from "@/styles/sections/softwares/SoftwareBanner.module.scss";
 
 // IMAGES //
+import SoftwareLogo from "../../../public/img/softwares/chronos-logo.png";
 
 // DATA //
 
 /** SoftwareBanner Section */
 export default function SoftwareBanner() {
-	return <section className={styles.SoftwareBanner}></section>;
+	return (
+		<section className={`${styles.SoftwareBanner} ptb_100`}>
+			<div className="container">
+				<img
+					src={SoftwareLogo.src}
+					alt="Software Logo"
+					className={`${styles.SoftwareLogo}`}
+				/>
+			</div>
+		</section>
+	);
 }
