79d0f5888ef4775a2d6a62162413499f8c3c663b
pramodchauhanting <108465153+pramodchauhanting@users.noreply.github.com>
2025-02-18 15:18:02 +0530

    section added

--- pages/softwares/chronos.js
+++ pages/softwares/chronos.js
@@ -5,6 +5,7 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import MetaTags from "@/components/MetaTags";
 import TestimonialFeedback from "@/components/TestimonialFeedback";
+import InnerBanner from "@/components/InnerBanner";
 
 // SECTIONS //
 import SoftwareBanner from "@/sections/softwares/SoftwareBanner";
@@ -12,6 +13,7 @@ import SoftwareMarket from "@/sections/softwares/SoftwareMarket";
 import Redefining from "@/sections/softwares/Redefining";
 import TrustedLeaders from "@/components/TrustedLeaders";
 import ServicesCircle from "@/components/ServicesCircle";
+import IntuitiveStepProcess from "@/sections/softwares/IntuitiveStepProcess";
 
 // PLUGINS //
 
@@ -21,6 +23,7 @@ import ServicesCircle from "@/components/ServicesCircle";
 import styles from "@/styles/pages/softwares/Software.module.scss";
 
 // IMAGES //
+import desktop_banner from "@/../public/img/banner/desktop_banner.jpg";
 
 // DATA //
 
@@ -36,12 +39,20 @@ export default function SoftwarePage() {
 
 			{/* Page Content starts here */}
 			<main className={styles.SoftwarePage}>
+				{/* <InnerBanner
+					bannerTitle="Energy solutions for those who see beyond the grid"
+					bannerDescription="Aurora empowers industries with tailored energy intelligence, helping decision-makers drive impact, manage risks, and seize opportunities in a rapidly changing energy landscape."
+					desktopImage={desktop_banner.src}
+					mobileImage={desktop_banner.src}
+					videoSrc="../../img/softwares/frame_video.mp4"
+				/> */}
 				<SoftwareBanner />
-				{/* <Redefining />
+				<IntuitiveStepProcess />
+				<Redefining />
 				<SoftwareMarket />
 				<TrustedLeaders />
-				<TestimonialFeedback /> */}
-				<ServicesCircle />
+				<TestimonialFeedback />
+				{/* <ServicesCircle /> */}
 			</main>
 			{/* Page Content ends here */}
 
