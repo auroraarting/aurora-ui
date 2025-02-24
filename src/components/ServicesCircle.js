63c4670d4eb31ca10e18da0b56d462c73d316b0b
Pawan201296 <pawansingh261094@gmail.com>
2025-02-24 21:08:00 +0530

    circular sec

--- src/components/ServicesCircle.js
+++ src/components/ServicesCircle.js
@@ -12,8 +12,17 @@
 import styles from "@/styles/components/ServicesCircle.module.scss";
 
 // IMAGES //
+import IconStrategy from "../../public/img/softwares/Icon-Strategy.svg";
 
 // DATA //
+const services = [
+	{ id: 1, icon: IconStrategy.src, label: "Strategy" },
+	{ id: 2, icon: IconStrategy.src, label: "Execution" },
+	{ id: 3, icon: IconStrategy.src, label: "Innovation" },
+	{ id: 4, icon: IconStrategy.src, label: "Growth" },
+	{ id: 5, icon: IconStrategy.src, label: "Optimization" },
+	{ id: 6, icon: IconStrategy.src, label: "Sustainability" },
+];
 
 /** ServicesCircle Component */
 export default function ServicesCircle() {
@@ -23,7 +32,7 @@ export default function ServicesCircle() {
 				<div className={`${styles.CircleGrid}`}>
 					<div className={`${styles.CircleInfo}`}>
 						<h3 className="text_xl color_white pb_20">
-							Chronos Edge in energy storage valuation
+							Chronos Edge in Energy Storage Valuation
 						</h3>
 						<p className="text_reg color_platinum_gray">
 							Lorem ipsum dolor sit amet consectetur. Velit vel iaculis fames velit
@@ -31,7 +40,26 @@ export default function ServicesCircle() {
 							mi eu urna risus nulla.
 						</p>
 					</div>
-					<div className={`${styles.CircleInfo} ${styles.CircleBox}`}></div>
+					<div className={`${styles.CircleInfo} ${styles.CircleBox}`}>
+						{/* Quadrants */}
+						<div className={`${styles.Quadrant} ${styles.TopLeft}`}>
+							<img src={IconStrategy.src} alt="Strategy" className={styles.Icon} />
+							{/* <p className={`${styles.IconInfo}`}>
+								Lorem ipsum dolor sit amet consectetur. Sed dolor purus elit.
+							</p> */}
+						</div>
+						<div className={`${styles.Quadrant} ${styles.TopRight}`}>
+							<img src={IconStrategy.src} alt="Strategy" className={styles.Icon} />
+						</div>
+						<div className={`${styles.Quadrant} ${styles.BottomLeft}`}>
+							<img src={IconStrategy.src} alt="Strategy" className={styles.Icon} />
+						</div>
+						<div className={`${styles.Quadrant} ${styles.BottomRight}`}>
+							<img src={IconStrategy.src} alt="Strategy" className={styles.Icon} />
+						</div>
+						{/* Inner Circle */}
+						<div className={`${styles.CenterBox}`}></div>
+					</div>
 				</div>
 			</div>
 		</div>
