729cb9e47527deadc36461bce5de226e8a06e28f
pramodchauhanting <pramod.chouhan@ting.in>
2025-02-24 19:06:34 +0530

    page created

---
+++ pages/how-we-help/transactions.js
@@ -0,0 +1,89 @@
+// MODULES //
+import { useState } from "react";
+
+// COMPONENTS //
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import MetaTags from "@/components/MetaTags";
+import TestimonialFeedback from "@/components/TestimonialFeedback";
+import SectionsHeader from "@/components/SectionsHeader";
+import Button from "@/components/Buttons/Button";
+import TrustedLeaders from "@/components/TrustedLeaders";
+import Insights from "@/components/Insights";
+
+// SECTIONS //
+
+// PLUGINS //
+
+// UTILS //
+
+// STYLES //
+import styles from "@/styles/pages/how-we-help/Transactions.module.scss";
+
+// IMAGES //
+import desktop_banner from "@/../public/img/financial-sector/desktop_banner.jpg";
+
+// DATA //
+
+/** Transactions Page */
+export default function Advisory() {
+	const [isFormVisible, setIsFormVisible] = useState(false); // Form hidden by default
+
+	/** scrollToSection */
+	const scrollToSection = (id) => {
+		scroller.scrollTo(id, {
+			duration: 500,
+			smooth: true,
+			offset: -100,
+			spy: true,
+			onEnd: () => console.log("Scrolling finished!"), // ❌ Not available directly
+		});
+
+		setTimeout(() => {
+			setIsFormVisible(true);
+			console.log("Scrolling finished!");
+		}, 500);
+	};
+
+	const headerArray = [
+		{ name: "Expertise", id: "#expertise" },
+		{ name: "Available Regions", id: "#availableregions" },
+		{ name: "Why Aurora", id: "#whyaurora" },
+		{ name: "Clients", id: "#clients" },
+		<div key="btn" to="Insights" onClick={() => scrollToSection("Insights")}>
+			<Button color="primary" variant="filled" shape="rounded">
+				Get Started
+			</Button>
+		</div>,
+	];
+	return (
+		<div>
+			{/* Metatags */}
+			<MetaTags
+				Title={"Transactions"}
+				Desc={""}
+				OgImg={""}
+				Url={"/transactions"}
+			/>
+
+			{/* Header */}
+			{/* <Header /> */}
+
+			{/* Page Content starts here */}
+			<main className={styles.TransactionsPage}>
+				<SectionsHeader data={headerArray} />
+
+				<TrustedLeaders />
+				<TestimonialFeedback />
+				<Insights
+					isFormVisible={isFormVisible}
+					setIsFormVisible={setIsFormVisible}
+				/>
+			</main>
+			{/* Page Content ends here */}
+
+			{/* Footer */}
+			{/* <Footer /> */}
+		</div>
+	);
+}
