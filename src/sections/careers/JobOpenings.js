67f527e3ce6e5f2e63efa8719f2f41bb7343cbcd
pramodchauhanting <pramod.chouhan@ting.in>
2025-03-17 19:18:23 +0530

    job opening section created

--- src/sections/careers/JobOpenings.js
+++ src/sections/careers/JobOpenings.js
@@ -6,11 +6,6 @@ import Button from "@/components/Buttons/Button";
 // SECTIONS //
 
 // PLUGINS //
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import { Pagination, Navigation, Autoplay } from "swiper/modules";
 
 // UTILS //
 
