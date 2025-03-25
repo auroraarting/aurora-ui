512ced33b5cd4d16f9c4b82d52d2269ace5def17
pramodchauhanting <108465153+pramodchauhanting@users.noreply.github.com>
2025-03-25 17:31:30 +0530

    section added

--- src/sections/media-center/Leaders.js
+++ src/sections/media-center/Leaders.js
@@ -11,13 +11,13 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { Navigation } from "swiper/modules";
+import { Navigation, Autoplay } from "swiper/modules";
 import parse from "html-react-parser";
 
 // UTILS //
 
 // STYLES //
-import styles from "@/styles/sections/events/Speakers.module.scss";
+import styles from "@/styles/sections/media-center/Learders.module.scss";
 
 // IMAGES //
 import management_img from "../../../public/img/events/management_img.jpg";
@@ -30,8 +30,8 @@ import clock from "../../../public/img/icons/clock.svg";
 
 // DATA //
 
-/** Speakers Section */
-export default function Speakers() {
+/** Leaders Section */
+export default function Leaders() {
 	const [isPopupOpen, setIsPopupOpen] = useState(false);
 	const [slideNo, setSlideNo] = useState(0);
 
@@ -56,7 +56,7 @@ export default function Speakers() {
 		}
 	}, [slideNo]);
 
-	const eventSpeakersData = [
+	const mediaLeadersData = [
 		{
 			name: "Anna Clunes1",
 			designation: "British Ambassador, Poland1",
@@ -101,46 +101,96 @@ export default function Speakers() {
 			thumbnail: management_img.src,
 			sessions: [],
 		},
+		{
+			name: "Anna Clunes3",
+			designation: "British Ambassador, Poland3",
+			desc:
+				"Anna Clunes has served as His Majesty’s Ambassador to Poland since September 2020, bringing decades of diplomatic and leadership experience to the role.  Before her appointment, Anna held several senior positions in the UK government, including Acting Director General and Director at the Department for Exiting the European Union (2017–2020). Her expertise in economic diplomacy was honed as Director of Economic Diplomacy at the Foreign & Commonwealth Office (FCO) from 2015 to 2017.  Anna has also led strategic and protocol-focused departments, serving as Director of Protocol (2012–2014) and Head of Communications and Engagement (2010–2012) at the FCO, where she co-headed the Communications Directorate. <br /> Her experience extends to the Cabinet Office, where she was Deputy Director of the European and Global Issues Secretariat (2007–2009) and Private Secretary to the Prime Minister for Africa and Development at No. 10 Downing Street (2006–2007).  Her international assignments have included roles as Counsellor for External Relations at the UK Permanent Representation to the EU in Brussels (2003–2005), First Secretary for Counter-Terrorism at the UK Mission to the United Nations in New York (2000–2003), and Second Secretary for Development Assistance and Economic affairs at the Department for International Development in Warsaw (1996–2000).",
+			thumbnail: management_img.src,
+			sessions: [],
+		},
+		{
+			name: "Anna Clunes3",
+			designation: "British Ambassador, Poland3",
+			desc:
+				"Anna Clunes has served as His Majesty’s Ambassador to Poland since September 2020, bringing decades of diplomatic and leadership experience to the role.  Before her appointment, Anna held several senior positions in the UK government, including Acting Director General and Director at the Department for Exiting the European Union (2017–2020). Her expertise in economic diplomacy was honed as Director of Economic Diplomacy at the Foreign & Commonwealth Office (FCO) from 2015 to 2017.  Anna has also led strategic and protocol-focused departments, serving as Director of Protocol (2012–2014) and Head of Communications and Engagement (2010–2012) at the FCO, where she co-headed the Communications Directorate. <br /> Her experience extends to the Cabinet Office, where she was Deputy Director of the European and Global Issues Secretariat (2007–2009) and Private Secretary to the Prime Minister for Africa and Development at No. 10 Downing Street (2006–2007).  Her international assignments have included roles as Counsellor for External Relations at the UK Permanent Representation to the EU in Brussels (2003–2005), First Secretary for Counter-Terrorism at the UK Mission to the United Nations in New York (2000–2003), and Second Secretary for Development Assistance and Economic affairs at the Department for International Development in Warsaw (1996–2000).",
+			thumbnail: management_img.src,
+			sessions: [],
+		},
 	];
-	// console.log(eventSpeakersData);
 	return (
-		<section className={`${styles.Speakers}`}>
+		<section className={`${styles.Leaders}`}>
 			<div className="container">
 				<div className={`${styles.titleWrapper}`}>
 					<h2 className="text_xl font_primary f_w_m color_secondary pb_10">
-						Past Speakers
+						Leaders
 					</h2>
 					<p className={`${styles.label} text_reg color_dark_gray`}>
-						Aurora’s events bring together industry leaders, policymakers, and experts
-						for insightful discussions and data-driven analysis, shaping the future of
-						energy.
+						Lorem ipsum dolor sit amet consectetur. Elit massa a ut malesuada.
+						Tincidunt pellentesque euismod morbi elit in tempor in. Ut elit in diam ut
+						a
 					</p>
 				</div>
 				<div className={`${styles.content_main_wrap} pt_40`}>
 					<div className={`${styles.box_wrap}`}>
-						{eventSpeakersData.map((item, ind) => {
-							return (
-								<div
-									className={`${styles.box_item}`}
-									key={ind}
-									onClick={(e) => handleSlideClick1(e, ind)}
-									data-slide={ind}
-								>
-									<div className={`${styles.thumbnailImg}`}>
-										<img src={item.thumbnail} className="b_r_20" alt="story img" />
-									</div>
-									<div className={`${styles.content} pt_20`}>
-										<h5 className="text_reg f_w_m color_white font_secondary">
-											{item.name}
-										</h5>
-										<p className="text_xs color_platinum_gray">{item.designation}</p>
-									</div>
-									<div className={`${styles.hoverEffect} pt_20`}>
-										<img src={hoverEffect.src} className="" alt=" img" />
-									</div>
-								</div>
-							);
-						})}
+						<Swiper
+							modules={[Navigation, Autoplay]}
+							slidesPerView={1}
+							spaceBetween={20}
+							grabCursor={true}
+							speed={500}
+							loop={true}
+							navigation={{
+								prevEl: "#customPrevCollaboration",
+								nextEl: "#customNextCollaboration",
+							}}
+							// autoplay={{
+							// 	delay: 3000,
+							// 	disableOnInteraction: false,
+							// }}
+							breakpoints={{
+								768: {
+									slidesPerView: 3,
+								},
+								992: {
+									slidesPerView: 4,
+								},
+							}}
+							className={styles.sliderLeaders}
+						>
+							{mediaLeadersData.map((item, ind) => {
+								return (
+									<SwiperSlide key={ind}>
+										<div
+											className={`${styles.box_item}`}
+											onClick={(e) => handleSlideClick1(e, ind)}
+											data-slide={ind}
+										>
+											<div className={`${styles.thumbnailImg}`}>
+												<img src={item.thumbnail} className="b_r_20" alt="story img" />
+											</div>
+											<div className={`${styles.content} pt_20`}>
+												<h5 className="text_reg f_w_m color_white font_secondary">
+													{item.name}
+												</h5>
+												<p className="text_xs color_platinum_gray">{item.designation}</p>
+											</div>
+											<div className={`${styles.hoverEffect} pt_20`}>
+												<img src={hoverEffect.src} className="" alt=" img" />
+											</div>
+										</div>
+									</SwiperSlide>
+								);
+							})}
+						</Swiper>
+					</div>
+					<div className={`${styles.arrowSectionLeaders} f_w_a_j_center`}>
+						<button className={`${styles.customPrev}`} id="customPrevCollaboration">
+							<img src={slider_arrow.src} alt="icon" />
+						</button>
+						<button className={styles.customNext} id="customNextCollaboration">
+							<img src={slider_arrow.src} alt="icon" />
+						</button>
 					</div>
 					{/*  */}
 				</div>
@@ -173,7 +223,7 @@ export default function Speakers() {
 										className={styles.slider}
 										ref={sliderRef}
 									>
-										{eventSpeakersData.map((item, ind) => (
+										{mediaLeadersData.map((item, ind) => (
 											<SwiperSlide className={`${styles.item}`} key={ind}>
 												<div className={`${styles.PopupItem}`}>
 													<div className={`${styles.BoxFlex} f_w`}>
@@ -244,51 +294,6 @@ export default function Speakers() {
 																	</div>
 																</div>
 															))}
-															{/* <div className={`${styles.sessionBox} f_w`}>
-															<div className={`${styles.sessionTime}`}>
-																<p
-																	className={`${styles.tag} text_xxs font_primary text_uppercase color_white`}
-																>
-																	1:30 PM
-																</p>
-															</div>
-															<div className={`${styles.sessionDescription}`}>
-																<h4 className="text_xs font_primary color_white f_w_s_b">
-																	Welcome and Opening Remarks
-																</h4>
-																<div className={`${styles.dateFlex} f_r_a_center pt_10`}>
-																	<p className="text_xs f_w_m color_silver_gray text_uppercase f_r_a_center">
-																		<img
-																			src={clock.src}
-																			className={`${styles.clock}`}
-																			alt="clock"
-																		/>
-																		<span>1:30 PM - 1:40 PM CET</span>
-																	</p>
-																	<p className="text_xs f_w_m color_silver_gray  f_r_a_center">
-																		<img
-																			src={location.src}
-																			className={`${styles.location}`}
-																			alt="location"
-																		/>
-																		<span>InterContinental Warsaw, Conference Room</span>
-																	</p>
-																</div>
-																<div className={`${styles.ClientFlex} f_r_a_center`}>
-																	<div className={`${styles.ClientLogo}`}>
-																		<img src={profile_pic.src} alt="pic" />
-																	</div>
-																	<div className={`${styles.ClientDescription}`}>
-																		<h5 className="text_xs font_primary color_white f_w_m">
-																			John Feddersen
-																		</h5>
-																		<p className="text_xxs color_silver_gray f_w_l">
-																			British Ambassador, Poland
-																		</p>
-																	</div>
-																</div>
-															</div>
-														</div> */}
 														</div>
 													)}
 												</div>
