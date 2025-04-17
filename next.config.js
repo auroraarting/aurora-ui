ec6288c55b73eac7b950f92eb27960417ed05f7c
Pawan201296 <pawansingh261094@gmail.com>
2025-04-17 22:35:29 +0530

    headers

--- next.config.js
+++ next.config.js
@@ -20,6 +20,7 @@ const nextConfig = {
 				img-src 'self' data: https://www.google.co.in/ https://www.googletagmanager.com/ https://cms.auroraer.com https://auroraer.mystagingwebsite.com https://img.youtube.com https://i.ytimg.com https://maps.googleapis.com https://maps.gstatic.com https://*.google.com https://*.ggpht.com;
 				font-src 'self' https://fonts.gstatic.com https://maps.gstatic.com;
 				connect-src 'self' https://f.clarity.ms/ https://cms.auroraer.com https://auroraer.mystagingwebsite.com https://analytics.google.com/ https://www.youtube.com https://maps.googleapis.com https://www.google.com/;
+				frame-src https://www.googletagmanager.com/ https://www.youtube.com https://www.youtube-nocookie.com/ https://www.google.com https://maps.google.com https://www.google.com/maps; // Allow Google Maps embeds
 				object-src 'none';
 				base-uri 'self';
 				form-action 'self';
@@ -35,6 +36,14 @@ const nextConfig = {
 					{
 						key: "X-Content-Type-Options",
 						value: "nosniff",
+					},
+					{
+						key: "Referrer-Policy",
+						value: "strict-origin-when-cross-origin",
+					},
+					{
+						key: "Permissions-Policy",
+						value: "geolocation=(), camera=(), microphone=()",
 					},
 				],
 			},
