1766f91cc1d65fd07207e1dab4dbc7459c2748f6
Pawan201296 <pawansingh261094@gmail.com>
2025-04-17 22:32:16 +0530

    headers

--- next.config.js
+++ next.config.js
@@ -36,10 +36,6 @@ const nextConfig = {
 						key: "X-Content-Type-Options",
 						value: "nosniff",
 					},
-					{
-						key: "Referrer-Policy",
-						value: "strict-origin-when-cross-origin",
-					},
 				],
 			},
 		];
