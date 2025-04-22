94ddf6d18db4d2b9cabea30704a1df8f36efc1b5
VipVishal007 <mvishu405@gmail.com>
2025-04-22 13:09:20 +0530

    config cms-production added

--- next.config.js
+++ next.config.js
@@ -14,12 +14,12 @@ const nextConfig = {
 						key: "Content-Security-Policy",
 						value: `
 				default-src 'self';
-				media-src 'self' https://cms.auroraer.com https://auroraer.mystagingwebsite.com;
+				media-src 'self' https://cms.auroraer.com https://auroraer.mystagingwebsite.com https://cms-production.auroraer.com;
 				script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.clarity.ms/ https://googleads.g.doubleclick.net/ https://connect.facebook.net/ https://www.googletagmanager.com/ https://www.youtube.com https://s.ytimg.com https://maps.googleapis.com https://maps.gstatic.com;
 				style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://maps.googleapis.com https://maps.gstatic.com;
-				img-src 'self' data: https://www.google.co.in/ https://www.googletagmanager.com/ https://cms.auroraer.com https://auroraer.mystagingwebsite.com https://img.youtube.com https://i.ytimg.com https://maps.googleapis.com https://maps.gstatic.com https://*.google.com https://*.ggpht.com;
+				img-src 'self' data: https://www.google.co.in/ https://www.googletagmanager.com/ https://cms.auroraer.com https://cms-production.auroraer.com https://auroraer.mystagingwebsite.com https://img.youtube.com https://i.ytimg.com https://maps.googleapis.com https://maps.gstatic.com https://*.google.com https://*.ggpht.com;
 				font-src 'self' https://fonts.gstatic.com https://maps.gstatic.com;
-				connect-src 'self' https://vimeo.com/ https://f.clarity.ms/ https://cms.auroraer.com https://auroraer.mystagingwebsite.com https://analytics.google.com/ https://www.youtube.com https://maps.googleapis.com https://www.google.com/;
+				connect-src 'self' https://vimeo.com/ https://f.clarity.ms/ https://cms.auroraer.com https://cms-production.auroraer.com https://auroraer.mystagingwebsite.com https://analytics.google.com/ https://www.youtube.com https://maps.googleapis.com https://www.google.com/;
 				frame-src https://www.googletagmanager.com/ https://www.youtube.com https://www.youtube-nocookie.com/ https://player.vimeo.com/ https://www.google.com https://maps.google.com https://www.google.com/maps; // Allow Google Maps embeds
 				object-src 'none';
 				base-uri 'self';
