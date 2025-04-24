d9e923b0151b22f97c9de4a5abf74aa0f26fae1f
Nihal Padwal <nihal.padwal@ting.in>
2025-04-24 12:36:25 +0530

    chg

---
+++ src/services/Insights.service.js
@@ -0,0 +1,36 @@
+import GraphQLAPI from "./Graphql.service";
+
+/** Insights Page */
+export const getInsights = async () => {
+	const query = `
+query GetInsights {
+  posts(first: 9999) {
+    nodes{
+      content
+        title
+        slug
+        categories(first: 9999) {
+          nodes {
+            slug
+            name
+          }
+        }
+        language {
+          id
+          code
+          language_code
+          native_name
+        }
+        tags(first: 9999) {
+          nodes {
+            name
+            slug
+          }
+        }
+    }
+  }
+}
+    `;
+	const res = await GraphQLAPI(query);
+	return res;
+};
