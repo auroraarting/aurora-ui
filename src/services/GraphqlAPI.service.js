3e418dbb81f1601c7730e3ba233332968a2d8b07
Sudip-Bhowmick <writetosb786@gmail.com>
2025-04-24 14:48:44 +0530

    data limit upgrade

---
+++ src/services/GraphqlAPI.service.js
@@ -0,0 +1,15 @@
+import { ClientHeaders, ServerHeaders } from "@/utils/RequestHeaders";
+
+/** GraphQLAPI with support for variables */
+export default async function GraphQLAPI(query, variables = {}) {
+	try {
+		const req = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
+			...ClientHeaders,
+			body: JSON.stringify({ query, variables }), // ✅ Send variables
+		});
+		const res = await req.json();
+		return res;
+	} catch (error) {
+		console.log("GraphQLAPI error:", error);
+	}
+}
