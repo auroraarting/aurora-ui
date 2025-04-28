d6369bd391e5d3d4b65b2440805a34515a36f933
Nihal Padwal <nihal.padwal@ting.in>
2025-04-28 22:16:25 +0530

    chg

--- pages/api/shortInsights.js
+++ pages/api/shortInsights.js
@@ -13,9 +13,9 @@ export default async function handler(req, res) {
 			getInsightsCategories(),
 			getInsights('first: 3, where: {categoryName: ""}'),
 		]);
-		const otherList = list?.data?.posts?.nodes;
+		const defaultList = list?.data?.posts?.nodes;
 		const countries = categoriesForSelect.data.countries.nodes;
-		res.status(200).json({ otherList, countries });
+		res.status(200).json({ data: defaultList, countries });
 	} catch (error) {
 		console.error("Error fetching insights results:", error);
 		res.status(500).json({ error: "Failed to fetch insights results" });
