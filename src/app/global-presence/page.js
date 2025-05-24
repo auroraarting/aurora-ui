8a0e4498b053268b781b1c7330ddf5447923d29a
Nihal Padwal <nihal.padwal@ting.in>
2025-05-24 16:06:57 +0530

    chg

--- src/app/global-presence/page.js
+++ src/app/global-presence/page.js
@@ -39,58 +39,63 @@ async function getData() {
 	// const regions = await getRegions();
 	// const page = await getGlobalPresencePage();
 	const [regions, page] = await Promise.all([
-		getRegions(),
+		// getRegions(),
 		getGlobalPresencePage(),
 	]);
 
-	const mapJson = getMapJsonForAllRegions(regions);
+	// const mapJson = getMapJsonForAllRegions(regions);
 
-	const regionsArr = regions?.data?.regions?.nodes?.map((item) => {
-		let obj = {};
+	// const regionsArr = regions?.data?.regions?.nodes?.map((item) => {
+	// 	let obj = {};
 
-		obj.title = item.name;
-		if (item.countries?.nodes.length > 0) {
-			obj.children = (
-				<div className={`${styles.CountryWrapper}`}>
-					<div className={`${styles.CountryBox}`}>
-						{item.countries?.nodes?.map((item2) => {
-							if (item2?.countries?.hideonglobalpresence) return <></>;
-							return (
-								<div className={`${styles.CountryItem}`} key={item2.slug}>
-									<a href={`/global-presence/${item2.slug}`}>
-										<img
-											src={
-												item2?.featuredImage?.node?.mediaItemUrl ||
-												item2?.countries?.bannerSection?.image?.node?.mediaItemUrl
-											}
-											className={`width_100 b_r_10 ${styles.image}`}
-											alt={item2.title}
-										/>
-										<div className="f_j a_center pt_10">
-											<h5 className="text_reg font_primary f_w_m color_secondary ">
-												{item2.title}
-											</h5>
-											<span>
-												<img
-													src={slider_arrow.src}
-													className={`${styles.icon}`}
-													alt="arrow"
-												/>
-											</span>
-										</div>
-									</a>
-								</div>
-							);
-						})}
-					</div>
-				</div>
-			);
-		}
-		return obj;
-	});
+	// 	obj.title = item.name;
+	// 	if (item.countries?.nodes.length > 0) {
+	// 		obj.children = (
+	// 			<div className={`${styles.CountryWrapper}`}>
+	// 				<div className={`${styles.CountryBox}`}>
+	// 					{item.countries?.nodes?.map((item2) => {
+	// 						if (item2?.countries?.hideonglobalpresence) return <></>;
+	// 						return (
+	// 							<div className={`${styles.CountryItem}`} key={item2.slug}>
+	// 								<a href={`/global-presence/${item2.slug}`}>
+	// 									<img
+	// 										src={
+	// 											item2?.featuredImage?.node?.mediaItemUrl ||
+	// 											item2?.countries?.bannerSection?.image?.node?.mediaItemUrl
+	// 										}
+	// 										className={`width_100 b_r_10 ${styles.image}`}
+	// 										alt={item2.title}
+	// 									/>
+	// 									<div className="f_j a_center pt_10">
+	// 										<h5 className="text_reg font_primary f_w_m color_secondary ">
+	// 											{item2.title}
+	// 										</h5>
+	// 										<span>
+	// 											<img
+	// 												src={slider_arrow.src}
+	// 												className={`${styles.icon}`}
+	// 												alt="arrow"
+	// 											/>
+	// 										</span>
+	// 									</div>
+	// 								</a>
+	// 							</div>
+	// 						);
+	// 					})}
+	// 				</div>
+	// 			</div>
+	// 		);
+	// 	}
+	// 	return obj;
+	// });
 
 	return {
-		props: { regions, page: page.data.page.globalPresence, mapJson, regionsArr },
+		props: {
+			regions,
+			page: {},
+			mapJson: {},
+			regionsArr: {},
+		},
 	};
 }
 
@@ -111,7 +116,7 @@ export default async function GlobalPresence() {
 			{/* <Header /> */}
 
 			{/* Page Content starts here */}
-			<GlobalPresenceWrap {...props} />
+			{/* <GlobalPresenceWrap {...props} /> */}
 			{/* Page Content ends here */}
 
 			{/* Footer */}
