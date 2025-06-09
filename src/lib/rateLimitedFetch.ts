3fed697106d4ee4321d6b45502ffb004fc91ffe5
Nihal Padwal <nihal.padwal@ting.in>
2025-06-09 18:38:56 +0530

    reduced time to 1s

--- src/lib/rateLimitedFetch.ts
+++ src/lib/rateLimitedFetch.ts
@@ -3,7 +3,7 @@ import Bottleneck from "bottleneck";
 
 const limiter = new Bottleneck({
 	maxConcurrent: 1, // One request at a time
-	minTime: 3000, // 20s delay between requests (3/min)
+	minTime: 1000, // 20s delay between requests (3/min)
 	reservoir: 3, // 3 requests
 	reservoirRefreshAmount: 3,
 	reservoirRefreshInterval: 60 * 1000, // every minute
