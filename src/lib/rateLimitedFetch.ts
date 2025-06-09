60becf95297deaf14f881b1f1122a6c0864b967f
Nihal Padwal <nihal.padwal@ting.in>
2025-06-09 19:56:41 +0530

    redis

--- src/lib/rateLimitedFetch.ts
+++ src/lib/rateLimitedFetch.ts
@@ -3,7 +3,8 @@ import Bottleneck from "bottleneck";
 import IORedis from "ioredis";
 
 const redis = new IORedis(
-	"redis://default:TSy692fg7tnqS8LLFd2rFfnsF2mDFDYI@redis-17052.c243.eu-west-1-3.ec2.redns.redis-cloud.com:17052"
+	"redis://default:TSy692fg7tnqS8LLFd2rFfnsF2mDFDYI@redis-17052.c243.eu-west-1-3.ec2.redns.redis-cloud.com:17052",
+	{ tls: { rejectUnauthorized: false } }
 );
 
 const limiter = new Bottleneck({
