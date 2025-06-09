84d366f1a26e1a0bc988c1cfe414734b8652f1ab
Nihal Padwal <nihal.padwal@ting.in>
2025-06-09 10:28:35 +0530

    chg

--- src/services/GlobalPresenceLanguages.service.js
+++ src/services/GlobalPresenceLanguages.service.js
@@ -138,166 +138,80 @@ export const getCountryInside = async (slug) => {
 	const query = `
 query GetCountryInside {
   countryBy(slug: "${decodeURIComponent(slug)}") {
-    translations{
-       slug
-    title
-    countries {
-      hideonglobalpresence
-      bannerSection {
-        description
-        title
-        videoLink
-        image {
-          node {
-            altText
-            mediaItemUrl
-          }
-        }
-        mobileImage {
-          node {
-            altText
-            mediaItemUrl
-          }
-        }
-      }
-      announcement {
-        slide {
-          thumbnailImage {
-            node {
-              altText
-              mediaItemUrl
-            }
-          }
-          videoLink
-          heading
+    translations {
+      slug
+      title
+      countries {
+        hideonglobalpresence
+        bannerSection {
           description
-          buttonText
-          buttonLink
-        }
-      }
-      introduction {
-        sectionTitle
-        tabTitle
-        description
-      }
-      ourOfferings {
-        sectionTitle
-        tabTitle
-      }
-      keyAdvantages {
-        title
-        tabTitle
-        description
-        advantages {
-          icon {
+          title
+          videoLink
+          image {
             node {
               altText
               mediaItemUrl
             }
           }
-          advantagesTitle
-          advantagesDescription
-        }
-      }
-      availableRegions {
-        sectionTitle
-        tabTitle
-        team(first: 9999) {
-          nodes {
-            ... on Team {
-              id
-              title
-              teams {
-                thumbnail {
-                  designation
-                  linkedinLink
-                  image {
-                    node {
-                      altText
-                      mediaItemUrl
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-      ourClients {
-        sectionTitle
-        tabTitle
-        selectLogos(first: 999) {
-          nodes {
-            ... on ClientsLogo {
-              id
-              featuredImage {
-                node {
-                  altText
-                  mediaItemUrl
-                }
-              }
-            }
-          }
-        }
-        testimonials(first: 999) {
-          nodes {
-            ... on Testimonial {
-              id
-              title
-              content
-              testimonials {
-                designation
-              }
-            }
-          }
-        }
-      }
-      eventsAndWebinars {
-        sectionTitle
-        tabTitle
-      }
-      insights {
-        sectionTitle
-        sectionDesc
-      }
-      subscribeSection {
-        description
-        sectionTitle
-        image {
-          node {
-            altText
-            mediaItemUrl
-          }
-        }
-      }
-      map {
-        zoom
-        countryPin {
-          lat
-          lng
-        }
-        markers {
-          mapThumbnail {
+          mobileImage {
             node {
               altText
               mediaItemUrl
             }
           }
-          category(first: 999) {
+        }
+        announcement {
+          slide {
+            thumbnailImage {
+              node {
+                altText
+                mediaItemUrl
+              }
+            }
+            videoLink
+            heading
+            description
+            buttonText
+            buttonLink
+          }
+        }
+        introduction {
+          sectionTitle
+          tabTitle
+          description
+        }
+        ourOfferings {
+          sectionTitle
+          tabTitle
+        }
+        keyAdvantages {
+          title
+          tabTitle
+          description
+          advantages {
+            icon {
+              node {
+                altText
+                mediaItemUrl
+              }
+            }
+            advantagesTitle
+            advantagesDescription
+          }
+        }
+        availableRegions {
+          sectionTitle
+          tabTitle
+          team(first: 9999) {
             nodes {
-              contentType {
-                node {
-                  name
-                }
-              }
-              ... on Service {
+              ... on Team {
                 id
-                slug
                 title
-                content
-                services {
-                  map {
-                    logo {
+                teams {
+                  thumbnail {
+                    designation
+                    linkedinLink
+                    image {
                       node {
                         altText
                         mediaItemUrl
@@ -306,111 +220,221 @@ query GetCountryInside {
                   }
                 }
               }
-              ... on Software {
-                id
-                title
-                slug
-                content
-                softwares {
-                  map {
-                    logo {
-                      node {
-                        altText
-                        mediaItemUrl
-                      }
-                    }
-                  }
-                }
-              }
-              ... on Product {
-                id
-                title
-                slug
-                content
-                products {
-                  map {
-                    logo {
-                      node {
-                        altText
-                        mediaItemUrl
-                      }
-                    }
-                  }
-                }
-              }
-              slug
             }
           }
-          coordinates {
+        }
+        ourClients {
+          sectionTitle
+          tabTitle
+          selectLogos(first: 999) {
+            nodes {
+              ... on ClientsLogo {
+                id
+                featuredImage {
+                  node {
+                    altText
+                    mediaItemUrl
+                  }
+                }
+              }
+            }
+          }
+          testimonials(first: 999) {
+            nodes {
+              ... on Testimonial {
+                id
+                title
+                content
+                testimonials {
+                  designation
+                }
+              }
+            }
+          }
+        }
+        eventsAndWebinars {
+          sectionTitle
+          tabTitle
+        }
+        insights {
+          sectionTitle
+          sectionDesc
+        }
+        subscribeSection {
+          description
+          sectionTitle
+          image {
+            node {
+              altText
+              mediaItemUrl
+            }
+          }
+        }
+        map {
+          zoom
+          countryPin {
             lat
             lng
           }
-          locationtitle
+          markers {
+            mapThumbnail {
+              node {
+                altText
+                mediaItemUrl
+              }
+            }
+            category(first: 999) {
+              nodes {
+                contentType {
+                  node {
+                    name
+                  }
+                }
+                ... on Service {
+                  id
+                  slug
+                  title
+                  content
+                  services {
+                    map {
+                      headerLogo {
+                        node {
+                          altText
+                          mediaItemUrl
+                        }
+                      }
+                      logo {
+                        node {
+                          altText
+                          mediaItemUrl
+                        }
+                      }
+                    }
+                  }
+                }
+                ... on Software {
+                  id
+                  title
+                  slug
+                  content
+                  softwares {
+                    map {
+                      headerLogo {
+                        node {
+                          altText
+                          mediaItemUrl
+                        }
+                      }
+                      logo {
+                        node {
+                          altText
+                          mediaItemUrl
+                        }
+                      }
+                    }
+                  }
+                }
+                ... on Product {
+                  id
+                  title
+                  slug
+                  content
+                  products {
+                    map {
+                      headerLogo {
+                        node {
+                          altText
+                          mediaItemUrl
+                        }
+                      }
+                      logo {
+                        node {
+                          altText
+                          mediaItemUrl
+                        }
+                      }
+                    }
+                  }
+                }
+                slug
+              }
+            }
+            coordinates {
+              lat
+              lng
+            }
+            locationtitle
+            bottomText
+          }
+        }
+        mapThumb {
+          node {
+            altText
+            mediaItemUrl
+          }
+        }
+        topSectionsButton {
+          buttonText
+          iframe
+          url
+          file {
+            node {
+              altText
+              mediaItemUrl
+            }
+          }
+        }
+        middleSectionsButton {
+          buttonText
+          iframe
+          url
+          file {
+            node {
+              altText
+              mediaItemUrl
+            }
+          }
+        }
+        keyAdvantageSectionsButton {
+          buttonText
+          iframe
+          url
+          file {
+            node {
+              altText
+              mediaItemUrl
+            }
+          }
+        }
+        availableRegionsSectionsButton {
+          buttonText
+          iframe
+          url
+          file {
+            node {
+              altText
+              mediaItemUrl
+            }
+          }
+        }
+        insightsSectionsButton {
+          buttonText
+          iframe
+          url
+          file {
+            node {
+              altText
+              mediaItemUrl
+            }
+          }
         }
       }
-      mapThumb {
+      featuredImage {
         node {
           altText
           mediaItemUrl
         }
       }
-      topSectionsButton {
-        buttonText
-        iframe
-        file {
-          node {
-            altText
-            mediaItemUrl
-          }
-        }
-      }
-      middleSectionsButton {
-        buttonText
-        iframe
-        file {
-          node {
-            altText
-            mediaItemUrl
-          }
-        }
-      }
-      keyAdvantageSectionsButton {
-        buttonText
-        iframe
-        file {
-          node {
-            altText
-            mediaItemUrl
-          }
-        }
-      }
-      availableRegionsSectionsButton {
-        buttonText
-        iframe
-        file {
-          node {
-            altText
-            mediaItemUrl
-          }
-        }
-      }
-      insightsSectionsButton {
-        buttonText
-        iframe
-        file {
-          node {
-            altText
-            mediaItemUrl
-          }
-        }
-      }
-    }
-    featuredImage {
-      node {
-        altText
-        mediaItemUrl
-      }
-    }
     }
   }
 }
