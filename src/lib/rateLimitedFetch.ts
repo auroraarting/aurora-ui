44dd64ebbebae6de4680fd4814569a272f219ce0
Nihal Padwal <nihal.padwal@ting.in>
2025-06-09 20:10:46 +0530

    removed redis

--- src/lib/rateLimitedFetch.ts
+++ src/lib/rateLimitedFetch.ts
@@ -1,11 +1,11 @@
 // lib/rateLimitedFetch.ts
 import Bottleneck from "bottleneck";
-import IORedis from "ioredis";
+// import IORedis from "ioredis";
 
-const redis = new IORedis(
-	"redis://default:TSy692fg7tnqS8LLFd2rFfnsF2mDFDYI@redis-17052.c243.eu-west-1-3.ec2.redns.redis-cloud.com:17052",
-	{ tls: { rejectUnauthorized: false } }
-);
+// const redis = new IORedis(
+// 	"redis://default:aKAwsBVnsChBew7oL7yC+LLsHDTWHZHDvRzOD8aOUk+/ivR8/xMGWJmlfZArYD+8A436NkfKd5J/0IYY@64.227.145.242:6379",
+// 	{ tls: { rejectUnauthorized: false } }
+// );
 
 const limiter = new Bottleneck({
 	maxConcurrent: 3, // Allow up to 3 requests in parallel
@@ -13,10 +13,10 @@ const limiter = new Bottleneck({
 	reservoir: 3, // 3 requests per interval
 	reservoirRefreshAmount: 3, // Refill 3 requests
 	reservoirRefreshInterval: 1000, // Every 1 second
-	datastore: "ioredis",
-	clearDatastore: false,
-	clientOptions: {}, // Not used with ioredis, but required by Bottleneck
-	Redis: redis,
+	// datastore: "ioredis",
+	// clearDatastore: false,
+	// clientOptions: {}, // Not used with ioredis, but required by Bottleneck
+	// Redis: redis,
 });
 
 /** rateLimitedFetch  */
