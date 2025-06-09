a7e009433d1bd6a53de62a06ec9733ea66b1fde4
Nihal Padwal <nihal.padwal@ting.in>
2025-06-09 22:56:50 +0530

    chg

--- src/app/careers/faq/page.js
+++ src/app/careers/faq/page.js
@@ -38,7 +38,6 @@ export default async function Faq() {
 	return (
 		<div>
 			{/* Metatags */}
-			{/* <MetaTags Title={"Faq"} Desc={""} OgImg={""} Url={"/careers/faq"} /> */}
 
 			{/* Header */}
 			{/* <Header /> */}
