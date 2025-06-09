ca6c18bcfe7dbb66afae5315f41d8b8970f89b89
Nihal Padwal <nihal.padwal@ting.in>
2025-06-09 18:41:37 +0530

    config

--- src/lib/rateLimitedFetch.ts
+++ src/lib/rateLimitedFetch.ts
@@ -2,11 +2,11 @@
 import Bottleneck from "bottleneck";
 
 const limiter = new Bottleneck({
-	maxConcurrent: 1, // One request at a time
-	minTime: 1000, // 20s delay between requests (3/min)
+	maxConcurrent: 3, // One request at a time
+	minTime: 0, // 20s delay between requests (3/min)
 	reservoir: 3, // 3 requests
 	reservoirRefreshAmount: 3,
-	reservoirRefreshInterval: 60 * 1000, // every minute
+	reservoirRefreshInterval: 1000, // every minute
 });
 
 /** rateLimitedFetch  */
