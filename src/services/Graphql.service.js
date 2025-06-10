ee922124c8f2bfafa4eb33e07d1704d505a74358
Nihal Padwal <nihal.padwal@ting.in>
2025-06-10 11:54:42 +0530

    Chg

--- src/services/Graphql.service.js
+++ src/services/Graphql.service.js
@@ -18,3 +18,22 @@ export default async function GraphQLAPI(query) {
 		console.log(error, req, "errror");
 	}
 }
+
+/** GraphQLAPI  */
+export async function GraphQLAPINoBottleneck(query) {
+	let res;
+	let req;
+
+	try {
+		req = await fetch(`${process.env.API_URL}`, {
+			...ServerHeaders,
+			body: JSON.stringify({ query }),
+			next: { revalidate: 60 },
+		});
+		res = await req.json();
+		return res;
+	} catch (error) {
+		// req = await req.text();
+		console.log(error, req, "errror");
+	}
+}
