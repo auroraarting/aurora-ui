05b25512f734f522e0a896d5dd662cca824c044c
Pawan201296 <pawansingh261094@gmail.com>
2025-06-13 17:14:37 +0530

    seo

--- src/app/careers/life-at-aurora/page.js
+++ src/app/careers/life-at-aurora/page.js
@@ -29,12 +29,26 @@ import {
 } from "@/services/Insights.service";
 import { getOffices } from "@/services/Offices.service";
 import { getEarlyCareersListing } from "@/services/EarlyCareers.service";
+import { getPageSeo } from "@/services/Seo.service";
 
-/** Meta Data */
-export const metadata = {
-	title: "Life at Aurora | Aurora",
-	description: "Aurora",
-};
+/** generateMetadata  */
+export async function generateMetadata() {
+	const meta = await getPageSeo('page(id: "life-at-aurora", idType: URI)');
+	const seo = meta?.data?.page?.seo;
+
+	return {
+		title: seo?.title || "Default Title",
+		description: seo?.metaDesc || "Default description",
+		keywords: seo?.metaKeywords || "Default description",
+		openGraph: {
+			images: [
+				{
+					url: "https://www-staging.auroraer.com/img/og-image.jpg",
+				},
+			],
+		},
+	};
+}
 
 export const revalidate = 60; // Revalidates every 60 seconds
 
