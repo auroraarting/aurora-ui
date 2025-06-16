ffcc9ffec710822620e2427103372446f64c486e
Nihal Padwal <nihal.padwal@ting.in>
2025-06-16 18:44:22 +0530

    chg

--- src/app/layout.js
+++ src/app/layout.js
@@ -55,6 +55,14 @@ export default async function RootLayout({ children }) {
 				/>
 			</head>
 			<body>
+				<noscript>
+					<iframe
+						src="https://www.googletagmanager.com/ns.html?id=GTM-T74CK9L2"
+						height="0"
+						width="0"
+						style={{ display: "none", visibility: "hidden" }}
+					></iframe>
+				</noscript>
 				<Script
 					async
 					src="https://www.googletagmanager.com/gtag/js?id=G-Z75SWZL0J6"
@@ -65,6 +73,15 @@ export default async function RootLayout({ children }) {
                       gtag('js', new Date());
                       gtag('config', 'G-Z75SWZL0J6');`}
 				</Script>
+				{/* Google Tag Manager */}
+				<Script id="google-tag-manager" strategy="afterInteractive">
+					{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','GTM-T74CK9L2');`}
+				</Script>
+				{/* End Google Tag Manager */}
 
 				<GlobalContext>
 					{/* Header */}
