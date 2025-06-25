cfeca0ef9c4e357f8a09ffd641169046552e08b4
Nihal Padwal <nihal.padwal@ting.in>
2025-06-25 15:31:38 +0530

    chg

--- src/app/loading.js
+++ src/app/loading.js
@@ -3,5 +3,6 @@ import Loader from "@/components/Loader";
 /** Loading  */
 export default function Loading() {
 	// You can add any UI inside Loading, including a Skeleton.
-	return <Loader />;
+	// return <Loader />;
+	return <></>;
 }
