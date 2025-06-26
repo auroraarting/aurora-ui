4978510fdaaf313c02aeaecf6c167c6f6b54bd0f
Pawan201296 <pawansingh261094@gmail.com>
2025-06-26 17:59:57 +0530

    clarity added in website

--- src/app/layout.js
+++ src/app/layout.js
@@ -88,6 +88,14 @@ export default async function RootLayout({ children }) {
 					strategy="beforeInteractive"
 					src="https://cdn-cookieyes.com/client_data/c6fa123059758f90cec26dbf/script.js"
 				/>
+
+				<Script id="ms-clarity" strategy="afterInteractive">
+					{`(function(c,l,a,r,i,t,y){
+						c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+						t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+						y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+					})(window, document, "clarity", "script", "s5h7yx11f3");`}
+				</Script>
 			</head>
 			<body>
 				<noscript>
