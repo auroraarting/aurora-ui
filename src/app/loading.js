d43371aa67bedbe10686848e67133094c34afb06
Nihal Padwal <nihal.padwal@ting.in>
2025-06-26 11:58:53 +0530

    chg

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,8 +1,14 @@
 import Loader from "@/components/Loader";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 /** Loading  */
 export default function Loading() {
 	// You can add any UI inside Loading, including a Skeleton.
-	return <Loader />;
-	// return <></>;
+	// return <Loader />;
+	return (
+		<div className="Skeleton">
+			<Skeleton count={1} height={"100vh"} duration={0.8} />
+		</div>
+	);
 }
