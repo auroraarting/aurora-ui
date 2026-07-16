56fa14fbeec1985a2de1704b355d3cf10b1f7c33
nihal <254448365+ting-nihal_gigl@users.noreply.github.com>
2026-07-16 19:12:41 +0530

    added

--- src/app/resources/videos/[slug]/page.js
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-
-export const revalidate = 3600;
-
-/** Redirect video slugs to videos listing */
-export default function VideoSlug() {
-	redirect("/resources/videos");
-}
